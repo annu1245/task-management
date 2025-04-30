ea4ece798607cd5a92f3f00991a6c8f1d249fa24
Annu Poddar <annupoddar444@gmail.com>
2025-04-30 18:43:11 +0530

    Fixed client url from .env, took server port from .env

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,32 @@
 const express = require("express");
 require('dotenv').config();
 const databseConnection = require("./src/databse.js")
-const app = express();
-const port = 3000;
+const userRouter = require("./src/routes/user.router.js");
+const taskRouter = require("./src/routes/task.router.js");
 const cors = require("cors");
-
 const cookieParser = require("cookie-parser");
 
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
+
 app.use(cookieParser());
 app.use(express.json());
 
 databseConnection()
-.then(() => {
-  console.log("database is connected")
-})
-.then(() => {
-  app.listen(port, function(err){
-    if(err) console.log(err)
-    console.log("app is listning on port :", port)
+  .then(() => {
+    console.log("database is connected")
   })
-})
-
-const userRouter = require("./src/routes/user.router.js");
-const taskRouter = require("./src/routes/task.router.js");
+  .then(() => {
+    app.listen(port, function (err) {
+      if (err) console.log(err)
+      console.log("app is listning on port :", port)
+    })
+  })
 
 app.use(userRouter);
 app.use("/task", taskRouter);
